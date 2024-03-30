fa205b3054a3d5f570c612baa06c18f48548b080
Felipedgws <felipe.augusto1202@gmail.com>
2024-03-30 00:09:08 -0300

    a
    a

--- script.js
+++ script.js
@@ -7,26 +7,42 @@ const senha = document.getElementById("senha")
 
 if(nome.value == ""){
     window.alert("O campo nome está vazio!")
+    return
 }
 
 if(sobrenome.value == ""){
     window.alert("O campo sobrenome está vazio!")
+    return
 }
 
 if(email.value == "" || !valemail(email.value)){
     window.alert("O campo email está vazio ou preenchido errado!")
+    return
 }
 
-function valemail(email){
+if(!valsenha(senha.value, 8)){
+    window.alert("O campo senha deve ser preenchido com no mínimo 8  digitos!")
+    return
+}
+
+function valemail(emailinput){
     const emailregex = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/)
-}
 
-if(emailregex.test(email)){
+
+if(emailregex.test(emailinput)){
     return true
 }
+return false
 
 }
 
-if(senha.value == ""){
-    window.alert("O campo senha está vazio!")
+function valsenha(password, minDigits){
+    if(password.length >= minDigits){
+        return true
+    }
+    return false
 }
+
+}
+
+
